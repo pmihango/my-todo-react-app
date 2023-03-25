431fd2bff6c4c0ec0c50ce9fdaf6aa9e2eb098d7
pmihango <pmihango@gmail.com>
2023-03-25 16:00:43 +0300

    Added Todolist component

---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './Todo';
+
+function TodoList(props) {
+  return (
+    <div>
+      {props.todos.map(todo => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
